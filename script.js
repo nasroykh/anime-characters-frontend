04e33e00070cbdbe3b7534e3f2dbeaa98d653868
Nasreddine Yakhou <nasro_ykh@hotmail.com>
2021-01-28 18:45:57 +0100

    added mobile swipe feature

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-//https://kitsu.io/api/edge/anime?filter[text]=naruto
-
 let charInfos = $("#char-infos");
 let characters = [];
 let max;
@@ -60,9 +58,6 @@ $(document).ready(() => {
 		
 	});
 
-
-	
-
 	let base = 0;
 	$('#next-btn').click(function (e) { 
 		e.preventDefault();
@@ -80,4 +75,59 @@ $(document).ready(() => {
 	});
 });
 
-/* 7509 > 7518 */
+
+
+
+
+// SWIPE SCRIPT 
+
+
+
+
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+			/* left swipe */ 
+			$('#next-btn').trigger('click')
+        } else {
+			$('#prev-btn').trigger('click');
+            /* right swipe */
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            /* up swipe */ 
+        } else { 
+            /* down swipe */
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
